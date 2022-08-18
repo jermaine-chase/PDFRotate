b4fd8d508e8a465453d19acd6f52c7fdeba37716
Jermaine Chase <Jermaine.Chase@bcbsnc.com>
2022-08-18 08:52:03 -0400

    Update to rename PDFs after rotating

--- src/main/java/com/jerms/App.java
+++ src/main/java/com/jerms/App.java
@@ -1,6 +1,7 @@
 package com.jerms;
 
 import com.itextpdf.text.DocumentException;
+import com.jerms.file.FileRename;
 import com.jerms.pdf.PdfRotate;
 
 import java.io.File;
@@ -8,7 +9,7 @@ import java.io.IOException;
 
 public class App
 {
-    final static String sourceFolder = "/Users/u565341/Desktop/SBCs/Individual SBCs/URL SBCs/";
+    final static String sourceFolder = "C:\\Users\\u546546\\SBCs to be rotated\\";
     // final static String sourceFolder = "/Users/JermS/Desktop/PDF/";
     final static String destinationFolder = sourceFolder + "dest/";
     public static void main( String[] args )
@@ -21,12 +22,15 @@ public class App
     public static void listFilesForFolder(final File folder) {
         for (final File fileEntry : folder.listFiles()) {
             if (!fileEntry.isDirectory()) {
-                try {
-                    PdfRotate.rotate(fileEntry.getAbsolutePath(), destinationFolder + fileEntry.getName());
-                } catch (IOException | DocumentException e) {
-                    e.printStackTrace();
+                if (fileEntry.getName().endsWith("pdf")) {
+                    try {
+                        PdfRotate.rotate(fileEntry.getAbsolutePath(), destinationFolder + fileEntry.getName());
+                    } catch (IOException | DocumentException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         }
+        FileRename.rename(destinationFolder);
     }
 }
